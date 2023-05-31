98cd8324acd0436cf910855fce00bc8338717116
qwynbleid <andriuryabui@gmail.com>
2023-05-31 17:33:58 +0300

    Left-menu completed

--- app/src/main/java/com/example/myapp/ui/main/NotesFragment.kt
+++ app/src/main/java/com/example/myapp/ui/main/NotesFragment.kt
@@ -20,6 +20,7 @@ import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.Query
+import java.util.*
 
 class NotesFragment : Fragment(R.layout.fragment_notes) {
 
@@ -29,7 +30,9 @@ class NotesFragment : Fragment(R.layout.fragment_notes) {
     private lateinit var firestore: FirebaseFirestore
     private lateinit var noteAdapter: FirestoreRecyclerAdapter<FirebaseModel, NoteViewHolder>
     private var currentSortOption: String = "creationDate" // Початкова опція сортування (creationDate)
+    private var currentUserNotes:  FirestoreRecyclerOptions<FirebaseModel>? = null/////////////
 
+    @SuppressLint("NotifyDataSetChanged")
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
@@ -65,18 +68,19 @@ class NotesFragment : Fragment(R.layout.fragment_notes) {
 
             filterMenu.setNavigationItemSelectedListener {
                 when (it.itemId) {
+                    R.id.creation_date -> {
+                        handleSortOptionSelected("creationDate") // Запустити обробник сортування за полем "creationDate"
+                    }
+                    R.id.complete_date -> {
+                        handleSortOptionSelected("completeDate") // Запустити обробник сортування за полем "executionDate"
+                    }
                     R.id.priority -> {
                         handleSortOptionSelected("priority") // Запустити обробник сортування за полем "priority"
                     }
 //                    R.id.difficulty -> {
 //                        handleSortOptionSelected("difficulty") // Запустити обробник сортування за полем "difficulty"
 //                    }
-                    R.id.creation_date -> {
-                        handleSortOptionSelected("creationDate") // Запустити обробник сортування за полем "creationDate"
-                    }
-                    R.id.execution_date -> {
-                        handleSortOptionSelected("completeDate") // Запустити обробник сортування за полем "executionDate"
-                    }
+
                 }
                 drawerLayout.closeDrawer(filterMenu)
                 true
@@ -94,18 +98,65 @@ class NotesFragment : Fragment(R.layout.fragment_notes) {
 
             leftSideMenu.setNavigationItemSelectedListener {
                 when (it.itemId) {
-//                    R.id.dayBt -> {
-//                        TODO()
-//                    }
-//                    R.id.weekBt -> {
-//                        TODO()
-//                    }
-//                    R.id.monthBt -> {
-//                        TODO()
-//                    }
-//                    R.id.yearBt -> {
-//                        TODO()
-//                    }
+                    R.id.dayBt -> {
+                        val calendar = Calendar.getInstance()
+                        calendar.set(Calendar.HOUR_OF_DAY, 0)
+                        calendar.set(Calendar.MINUTE, 0)
+                        calendar.set(Calendar.SECOND, 0)
+                        calendar.set(Calendar.MILLISECOND, 0)
+                        val startOfDay = calendar.time
+
+                        calendar.add(Calendar.DAY_OF_MONTH, 1)
+                        val endOfDay = calendar.time
+
+                        updateQuery(startOfDay, endOfDay)
+                    }
+                    R.id.weekBt -> {
+                        val calendar = Calendar.getInstance()
+                        calendar.firstDayOfWeek = Calendar.MONDAY
+                        calendar.set(Calendar.DAY_OF_WEEK, Calendar.MONDAY)
+                        calendar.set(Calendar.HOUR_OF_DAY, 0)
+                        calendar.set(Calendar.MINUTE, 0)
+                        calendar.set(Calendar.SECOND, 0)
+                        calendar.set(Calendar.MILLISECOND, 0)
+                        val startOfWeek = calendar.time
+
+                        calendar.add(Calendar.WEEK_OF_YEAR, 1)
+                        val endOfWeek = calendar.time
+
+                        updateQuery(startOfWeek, endOfWeek)
+
+                    }
+                    R.id.monthBt -> {
+                        val calendar = Calendar.getInstance()
+                        calendar.set(Calendar.DAY_OF_MONTH, 1)
+                        calendar.set(Calendar.HOUR_OF_DAY, 0)
+                        calendar.set(Calendar.MINUTE, 0)
+                        calendar.set(Calendar.SECOND, 0)
+                        calendar.set(Calendar.MILLISECOND, 0)
+                        val startOfMonth = calendar.time
+
+                        calendar.add(Calendar.MONTH, 1)
+                        val endOfMonth = calendar.time
+
+                        updateQuery(startOfMonth, endOfMonth)
+
+                    }
+                    R.id.yearBt -> {
+                        val calendar = Calendar.getInstance()
+                        calendar.set(Calendar.DAY_OF_YEAR, 1)
+                        calendar.set(Calendar.HOUR_OF_DAY, 0)
+                        calendar.set(Calendar.MINUTE, 0)
+                        calendar.set(Calendar.SECOND, 0)
+                        calendar.set(Calendar.MILLISECOND, 0)
+                        val startOfYear = calendar.time
+
+                        calendar.add(Calendar.YEAR, 1)
+                        val endOfYear = calendar.time
+
+                        updateQuery(startOfYear, endOfYear)
+
+                    }
                     R.id.settingsBt -> {
 
                         val fragmentSettings = SettingsFragment()
@@ -125,11 +176,15 @@ class NotesFragment : Fragment(R.layout.fragment_notes) {
             .orderBy(currentSortOption, Query.Direction.ASCENDING)
 
         val userNotes = FirestoreRecyclerOptions.Builder<FirebaseModel>()
-            .setQuery(query, FirebaseModel::class.java).build()
+            .setQuery(query, FirebaseModel::class.java)
+            .build()
 
+        if (currentUserNotes  == null) {
+            currentUserNotes = userNotes
+        }
 
         noteAdapter =
-            object : FirestoreRecyclerAdapter<FirebaseModel, NoteViewHolder>(userNotes) {
+            object : FirestoreRecyclerAdapter<FirebaseModel, NoteViewHolder>(currentUserNotes!!) {
                 override fun onCreateViewHolder(
                     parent: ViewGroup,
                     viewType: Int
@@ -171,10 +226,32 @@ class NotesFragment : Fragment(R.layout.fragment_notes) {
         noteAdapter.startListening()
     }
 
+    @SuppressLint("NotifyDataSetChanged")
+    private fun updateQuery(startDate: Date, endDate: Date) {
+
+        val query = firestore.collection("Notes")
+            .document(user.uid)
+            .collection("UserNotes")
+            .whereGreaterThanOrEqualTo("completeDate", startDate)
+            .whereLessThan("completeDate", endDate)
+            .orderBy("completeDate")
+            .orderBy("priority")
+
+        val userNotes = FirestoreRecyclerOptions.Builder<FirebaseModel>()
+            .setQuery(query, FirebaseModel::class.java)
+            .build()
+
+        currentUserNotes = userNotes
+
+        noteAdapter.updateOptions(userNotes)
+        binding.recyclerView.adapter?.notifyDataSetChanged()
+    }
+
     private fun handleSortOptionSelected(sortOption: String) {
         currentSortOption = sortOption
         reloadRecyclerViewData()
     }
+
 
     @SuppressLint("NotifyDataSetChanged")
     private fun reloadRecyclerViewData() {
@@ -185,6 +262,7 @@ class NotesFragment : Fragment(R.layout.fragment_notes) {
 
         val userNotes = FirestoreRecyclerOptions.Builder<FirebaseModel>()
             .setQuery(query, FirebaseModel::class.java).build()
+
 
         noteAdapter.updateOptions(userNotes)
 
