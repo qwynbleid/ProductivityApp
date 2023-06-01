b15e11b055cab294c60279a4a495f364bcf492ef
infosystemAX <infosystemAX>
2023-06-01 18:23:33 +0300

    language change

--- app/src/main/java/com/example/myapp/ui/settings/SettingsFragment.kt
+++ app/src/main/java/com/example/myapp/ui/settings/SettingsFragment.kt
@@ -2,6 +2,7 @@ package com.example.myapp.ui.settings
 
 import android.app.AlertDialog
 import android.content.Context
+import android.content.SharedPreferences
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -14,11 +15,13 @@ import com.example.myapp.R
 import com.example.myapp.databinding.FragmentSettingsBinding
 import com.example.myapp.ui.login.SignInFragment
 import com.google.firebase.auth.FirebaseAuth
+import java.util.*
 
 class SettingsFragment: Fragment(R.layout.fragment_settings) {
 
     private lateinit var binding: FragmentSettingsBinding
     private lateinit var auth: FirebaseAuth
+    private lateinit var sharedPreferences: SharedPreferences
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -26,54 +29,100 @@ class SettingsFragment: Fragment(R.layout.fragment_settings) {
         val activity = requireActivity() as MainActivity
         activity.showMenu(false)
 
+
+
         binding = FragmentSettingsBinding.bind(view)
         auth = FirebaseAuth.getInstance()
+        sharedPreferences = requireContext().getSharedPreferences("Settings", Context.MODE_PRIVATE)
 
-
-        val sharedPreferences = requireContext().getSharedPreferences("Mode", Context.MODE_PRIVATE)
-        val editor = sharedPreferences.edit()
-        val nightMode = sharedPreferences.getBoolean("night", false)
-
-        if (nightMode) {
-            binding.themeSwitch.isChecked = true
-        }
 
         binding.apply {
+            languageButton.setOnClickListener {
+                // Открываем диалоговое окно с выбором языка
+                showLanguageSelectionDialog()
+            }
+
+            themeSwitch.isChecked = isNightModeEnabled()
 
             themeSwitch.setOnCheckedChangeListener { _, isChecked ->
-                if (!isChecked) {
-                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
-                    editor.putBoolean("night", false)
-                    editor.apply()
-                } else {
-                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
-                    editor.putBoolean("night", true)
-                    editor.apply()
-                }
+                setNightModeEnabled(isChecked)
             }
 
             logoutButton.setOnClickListener {
-
-                val alertDialog = AlertDialog.Builder(requireContext())
-                    .setMessage(R.string.sure_want_logout)
-                    .setPositiveButton(R.string.yes) { _, _ ->
-
-                        auth.signOut()
-
-                        val fragmentManager = requireActivity().supportFragmentManager
-                        fragmentManager.popBackStack(null, FragmentManager.POP_BACK_STACK_INCLUSIVE)
-
-                        val signInFragment = SignInFragment()
-                        fragmentManager.beginTransaction()
-                            .replace(R.id.fragmentContainer, signInFragment)
-                            .commit()
-
-                    }
-                    .setNegativeButton(R.string.no, null)
-                    .create()
-                alertDialog.show()
+                showLogoutConfirmationDialog()
             }
         }
+    }
+
+    private fun showLanguageSelectionDialog() {
+        val languageOptions = arrayOf("English", "Українська") // Варианты выбора языка
+        val currentLanguageIndex = if (isLanguageUkrainian()) 1 else 0 // Индекс текущего выбранного языка
+
+        val alertDialog = AlertDialog.Builder(requireContext())
+            .setTitle(R.string.hange_language)
+            .setSingleChoiceItems(languageOptions, currentLanguageIndex) { dialog, which ->
+                val selectedLanguage = when (which) {
+                    0 -> "en" // Английский язык
+                    1 -> "uk" // Украинский язык
+                    else -> "en" // По умолчанию выбран английский язык
+                }
+                changeLanguage(selectedLanguage)
+                dialog.dismiss()
+            }
+            .setNegativeButton(android.R.string.cancel, null)
+            .create()
+
+        alertDialog.show()
+    }
+
+    private fun isLanguageUkrainian(): Boolean {
+        val currentLocale = resources.configuration.locale
+        return currentLocale.language == "uk"
+    }
+
+    private fun changeLanguage(languageCode: String) {
+        val configuration = resources.configuration
+        val newLocale = Locale(languageCode)
+        configuration.setLocale(newLocale)
+        resources.updateConfiguration(configuration, resources.displayMetrics)
+        saveSelectedLanguage(languageCode)
+        requireActivity().recreate() // Пересоздаем активити, чтобы применить новый язык
+    }
+
+    private fun saveSelectedLanguage(languageCode: String) {
+        sharedPreferences.edit().putString("language", languageCode).apply()
+    }
+
+    private fun isNightModeEnabled(): Boolean {
+        return sharedPreferences.getBoolean("nightMode", false)
+    }
+
+    private fun setNightModeEnabled(enabled: Boolean) {
+        val nightMode = if (enabled) AppCompatDelegate.MODE_NIGHT_YES else AppCompatDelegate.MODE_NIGHT_NO
+        AppCompatDelegate.setDefaultNightMode(nightMode)
+        sharedPreferences.edit().putBoolean("nightMode", enabled).apply()
+    }
+
+    private fun showLogoutConfirmationDialog() {
+        val alertDialog = AlertDialog.Builder(requireContext())
+            .setMessage(R.string.sure_want_logout)
+            .setPositiveButton(R.string.yes) { _, _ ->
+                logout()
+            }
+            .setNegativeButton(R.string.no, null)
+            .create()
+
+        alertDialog.show()
+    }
+
+    private fun logout() {
+        auth.signOut()
+        val fragmentManager = requireActivity().supportFragmentManager
+        fragmentManager.popBackStack(null, FragmentManager.POP_BACK_STACK_INCLUSIVE)
+        val signInFragment = SignInFragment()
+        fragmentManager.beginTransaction()
+            .replace(R.id.fragmentContainer, signInFragment)
+            .commit()
     }
 
     override fun onCreateView(
