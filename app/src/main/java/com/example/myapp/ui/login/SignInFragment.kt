7c24db0d6d15679c6ac37aa8770f2c2304971ae4
qwynbleid <andriuryabui@gmail.com>
2023-06-02 11:42:23 +0300

    Bugs fixed

--- app/src/main/java/com/example/myapp/ui/login/SignInFragment.kt
+++ app/src/main/java/com/example/myapp/ui/login/SignInFragment.kt
@@ -27,7 +27,6 @@ class SignInFragment : Fragment(R.layout.fragment_sign_in) {
         auth = FirebaseAuth.getInstance()
         binding = FragmentSignInBinding.bind(view)
 
-
         binding.apply {
 
             signUpBt.setOnClickListener {
@@ -78,7 +77,6 @@ class SignInFragment : Fragment(R.layout.fragment_sign_in) {
                 auth.signOut()
             }
         }
-
     }
 
     override fun onCreateView(
